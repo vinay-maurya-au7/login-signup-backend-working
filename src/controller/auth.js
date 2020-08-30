47952777e3530c02cde4c0a3770734e1e7a0a1bf
vinay.maurya-au7 <vinay.shermauryan@gmail.com>
2020-08-31 00:03:12 +0530

    Authorization done

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -81,7 +81,14 @@ exports.signin = (req, res) => {
                 })
 }
 
-
+exports.requireSignin = (req, res, next) =>{
+    //use in postman ==> Headers -> add new key/value --> Authorization --> Bearer paste tokem
+    const token = req.headers.authorization.split(" ")[1];
+    console.log('looking for token',token)
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+}
 
 
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {signup, signin} = require('../controller/auth');
+const {signup, signin, requireSignin} = require('../controller/auth');
 
 router.post('/signup', signup);
 
 router.post('/signin', signin);
+
+router.post('/profile', requireSignin, (req, res) =>{
+    res.status(400).send({message: "hiii"})
+})
 
 module.exports = router;
